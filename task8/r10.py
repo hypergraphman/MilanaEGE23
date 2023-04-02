836de68720b42e083317d5b4df570f9212f1e7ab
Руслан Аитов <r_aitov@mail.ru>
2023-04-02 17:34:54 +0300

    Initial commit

---
+++ task8/r10.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+words = permutations('KAPKAN')
+s = set()
+for w in words:
+    word = ''.join(w)
+    if 'KK' not in word and 'AA' not in word:
+        s.add(word)
+print(len(s))
+print(k)
